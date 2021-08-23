a2e0b85f7bf054f06b79b7f43fe0798068d4e8c5
Adarsh999bh <adarshbhandary48@gmail.com>
2021-08-23 09:05:46 +0530

    Board class added

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,21 @@
+class Board{
+
+    public char gameBoard[];
+
+    Board(){
+        this.gameBoard=new char[10];
+    }
+    
+    public void initilizeBoardElements(){
+        for(int i=1;i<10;i++){
+            this.gameBoard[i]=" ".charAt(0);
+        }
+    }
+}
 public class TicTacToe{
     public static void main(String[] args) {
         System.out.println("welcome to tictactoe game");
+        Board b=new Board();
+        b.initilizeBoardElements();
     }
 }
